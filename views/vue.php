ede7e01968844d1efa4aeec11e8e8519dbb1038a
Elliot <elliot@raison.co>
2018-01-31 00:22:33 +0000

    Vue

--- views/vue.php
+++ views/vue.php
@@ -2,12 +2,17 @@
 <div id="app">
 	<h2>{{ welcome_message }}</h2>
 	<p>{{ welcome_description }}</p>
+
+	<hr />
+
+	<p>Please read the <a href="https://vuejs.org/v2/guide/">Vue.js guide</a> for more information.</p>
+
 </div>
 
 
 <script>
 
-	// get controller vars
+	// get vars from controller
 	var data = <?= json_encode($data) ?>
 
 	// create vue instance
@@ -16,7 +21,7 @@
 		data: data
 	})
 
-	// two methods to access data
+	// example: two methods to access data. Both reactive.
 	console.dir(data.welcome_message);
 	console.dir(vm.welcome_message);
 
